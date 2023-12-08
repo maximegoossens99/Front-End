ff45df58d09d152b0f10260aa2efbca0f54de647
Sacre Mukoko <sacremukoko@gmail.com>
2023-12-08 12:55:57 +0100

    removed thank

--- app.py
+++ app.py
@@ -49,35 +49,6 @@ collaborators = [
     {"name": "Michiel", "photo_path": "raw_data/Michiel.jpeg"},
 ]
 
-# Thank-you message
-st.write("## Thank You to Our Collaborators!")
-st.write("We appreciate the contributions of the following individuals:")
-
-# Display photos in three columns
-col1, col2, col3 = st.columns(3)
-
-for i, collaborator in enumerate(collaborators):
-    # Calculate the appropriate column based on the index
-    if i % 3 == 0:
-        current_col = col1
-    elif i % 3 == 1:
-        current_col = col2
-    else:
-        current_col = col3
-
-    # Display the collaborator's photo and thank-you message in the current column
-    with current_col:
-        st.image(
-            collaborator["photo_path"],
-            caption=collaborator["name"],
-            width=150,
-            use_column_width="auto",
-            channels="RGB",
-            output_format="JPEG",
-        )
-
-# Additional content and messages can be added here
-
 
 
 
